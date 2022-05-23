732cc2c75c273e9e12120f9481bdcde353cd2d1b
AleksandrBorzenko <santec777@gmail.com>
2022-05-23 22:45:47 +0700

    SceneManagament mechanics added

---
+++ Nav Mesh/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script manages a change of the scenes
+/// </summary>
+public class SceneTransition : MonoBehaviour
+{
+    private readonly string loadSceneName = "Load";
+
+    private int delay = 2000;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Loads the scene with transition
+    /// </summary>
+    public void LoadSceneWithTransition(string nextScene)
+    {
+        SceneManager.LoadSceneAsync(loadSceneName);
+        LoadNewScene(nextScene);
+    }
+
+    /// <summary>
+    /// Loads a game or menu scene with delay in load scene
+    /// </summary>
+    /// <param name="nextScene">Next scene name</param>
+    private async void LoadNewScene(string nextScene)
+    {
+        await Task.Delay(delay); // Here can be an initializing process
+        SceneManager.LoadSceneAsync(nextScene);
+        Destroy(gameObject);
+    }
+    
+}
